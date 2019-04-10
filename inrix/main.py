b7f5ed5cf95661ec1385b65a4565b76f562eda1d
Spencer Perry <spencerfarrallperry@gmail.com>
2019-04-10 07:08:53 -0400

    update parsing

--- inrix/main.py
+++ inrix/main.py
@@ -4,11 +4,13 @@
 #   main.py
 #
 
-import os
+import os, sys, argparse, shutil, gzip
 import pandas as pd
 import zipfile
 
-import argparse
+
+import urllib
+from smb.SMBHandler import SMBHandler
 
 from pathlib import Path
 
@@ -27,7 +29,11 @@ MONTHS = [
 'December'
 ]
 
-DATA_PATH = 'data/'
+BASE_PATH = 'DATA_ARCHIVE_DONT_TOUCH/s19_uep294/boston/ctps/INRIX/unzipped'
+LOCAL_PATH = '/Volumes/as_rsch_uds01\$/' + BASE_PATH
+TMP_DIR = 'temp/'
+
+
 
 conifg = None
 
@@ -45,32 +51,122 @@ def ingest_month():
 
     for i in range(0, 100):
 
-        f = readfile(i)
+        f = readfile(config.month, i)
+
         if f is not None:
-           month_frame = month_frame.append(f)
+            month_frame = month_frame.append(f)
+
+        sys.exit()
 
     return month_frame
 
 
-def unzip(filename):
 
-    with zipfile.ZipFile(filename, "r") as zip_ref:
-        zip_ref.extractall(DATA_PATH)
+#
+#   Return a file object from the remote smb server
+#
+
+def read_remote(path):
+
+    opener = urllib.request.build_opener(SMBHandler)
+    fh = opener.open('smb://sperry02:Winter2016@rstore1.uit.tufts.edu/as_rsch_uds01$/' + path)
+    data = fh.read()
+    fh.close()
+
+    return data
+
+#
+#   Copy the remote file object to the local file object
+#
+
+def copy_remote(remote_p, l_f):
+
+    data = read_remote(remote_p)
+    l_f.write(data)
+    print("Copied file: {} to: {}".format(remote_p, l_f.name))
+
+    l_f.close()
 
 
-def readfile(id):
+#
+#   Upload a local file object to the remote server
+#
 
-    filename = str(id).rjust(12, '0') + "-1"
-    file_path = os.path.join(DATA_PATH, filename)
+def upload(remote_path, l_f):
+
+    director = urllib.request.build_opener(SMBHandler)
+
+    # Upload by providing the `data` parameter with a file object
+    fh = director.open('smb://sperry02:Winter2016@rstore1.uit.tufts.edu/as_rsch_uds01$/' + remote_path, data=l_f)
+    fh.close()
+    l_f.close()
+
+
+
+def unzip(month, filename):
+
+    id = os.path.join(month, filename)
+    temp_p = os.path.join(TMP_DIR, id)
+
+    try:
+
+        remote_p = BASE_PATH + '/' + id
+        l_f = open(temp_p, 'wb+')
+
+        copy_remote(remote_p, l_f)
+
+        unzipped = gzip.open(temp_p, 'rb+')
+
+        temp_csv = open(temp_p + ".csv", 'wb+')
+
+        data = unzipped.read()
+
+        temp_csv.write(data)
+        temp_csv.seek(0)
+
+        upload(remote_p + ".csv", temp_csv)
+
+        # Cleanup
+
+        unzipped.close()
+        temp_csv.close()
+
+        os.remove(temp_p + ".csv")
+        os.remove(temp_p)
+
+
+    except Exception as e:
+        print(e)
+        print("Could Not Unzip File: {}".format(id))
+
+
+def temp(filepath):
+    pass
+
+
+
+
+#
+#   @param month :  unique month id (1-12)
+#   @param id    :  unique file id  (0-99)
+#
+
+def readfile(month, id):
+
+    month = str(month).rjust(2, '0') + "2015"
+    filename = str(id).rjust(12, '0')
 
     if config.unzip:
-        unzip(filename)
+        unzip(month, filename)
 
-    if Path(file_path).is_file():
-        return pd.read_csv(file_path)
-    else:
-        print("No file found for: {}".format(file_path))
-        return None
+    # file_path = os.path.join(LOCAL_PATH, month, filename + '.csv')
+
+    #if Path(file_path).is_file():
+    #    return None #pd.read_csv(file_path)
+    #else:
+    #    print("No file found for: {}".format(file_path))
+    #    return None
+
 
 
 
@@ -82,8 +178,8 @@ if __name__ == '__main__':
     global config
 
     parser = argparse.ArgumentParser(description='Process Inrix Data!')
-    parser.add_argument('month', help='Name of month of looking at')
-    parser.add_argument('--data',  default="data/" , help='Path to directory of source Inrix data  ')
+    parser.add_argument('month', help='Name of month of looking at', type=str)
+    parser.add_argument('--data',  default="data/", help='Path to directory of source Inrix data  ')
     parser.add_argument('--unzip', default=False, help='Whether to unzip the input files or not ')
 
     config = parser.parse_args()
